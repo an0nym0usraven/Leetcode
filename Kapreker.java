d1981a891cf980415eb138f5168f3f13ca2c3e0f
Aakanksha <73584586+an0nym0usraven@users.noreply.github.com>
2023-06-08 23:10:15 +0530

    Add files via upload

---
+++ Kapreker.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class Kapreker
+{
+    static boolean check(int n)
+    {
+        if(n==1)
+        {
+            return true;
+        }
+        int sq = n*n;
+        int count = 0;
+
+        while(sq!=0)
+        {
+            sq = sq/10;
+            count++;
+        }
+
+        sq = n*n;
+
+        for(int i=1; i<count; i++)
+        {
+            int eq = (int) Math.pow(10,i);
+            if(eq==n)
+            {
+                continue;
+            }
+            else
+            {
+                int sum = sq/eq + sq%eq;
+                if(sum == n)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+
+        System.out.println(num + " : " + check(num));
+    }
+}
